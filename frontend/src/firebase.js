e6076fbb125d92ead990a2dea0702547b6f85090
Marcondes <marcondesj.tir@gmail.com>
2025-03-13 16:56:00 -0300

    configurando firebase

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO",
-  projectId: "SEU_PROJETO_ID",
+  apiKey: "ede55b815743e25b8d9fe0aff3bacafba6f8f737",
+  authDomain: "firebase-adminsdk-fbsvc@trampofacil-55706.iam.gserviceaccount.com",
+  projectId: "trampofacil-55706",
   storageBucket: "SEU_STORAGE",
   messagingSenderId: "SEU_MENSAGEM_ID",
   appId: "SEU_APP_ID"
+  
 };
 
 const app = initializeApp(firebaseConfig);
